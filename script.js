f68d2d94b5ee54a3fa92dfc206f89a05bae43c82
niranj775 <niranj775@gmail.com>
2025-02-15 17:53:17 +0530

    Final App

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Test");
+let imgBox = document.getElementById('imgBox');
+let qrImg = document.getElementById('qrImage');
+let qrText = document.getElementById("qrText");
+
+function generateQR(){
+    if(qrText.value){
+   qrImg.src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data="+qrText.value;
+   imgBox.classList.add("show-img")
+} else{
+    qrText.classList.add("error");
+    setInterval(()=>{
+        qrText.classList.remove("error");
+    },1000);
+}
+}
